c9532a07b95d315b29d5ab77577624a137abbec0
thminggg <tsehongming21@gmail.com>
2024-09-24 03:09:29 -0700

    dev: update homepage design

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -9,45 +9,43 @@ interface IntroProps {
 
 function Intro({ id, className }: IntroProps) {
   return (
-    <section id={id} className={className}>
-      <div className="w-full flex items-center">
-        <div className="w-full md:w-[75%]">
-          <p className="slogan">
-            Hi,
-            <br />
-            I'm <span className="highlight">Patrick</span>
-            <br />
-            Software Engineer
-          </p>
-          <a
-            href="#contact"
-            className="contact mt-6 mb-6"
-            onClick={(e) => handleLinkClick(e, "#contact")}
-          >
-            Contact
-          </a>
-          <div className="flex gap-3 mt-3">
+    <section id={id} className={`${className} bg-container`}>
+      <div className="bg"></div>
+      <div className="content">
+        <div className="w-full flex items-center">
+          <div className="w-full">
+            <p className="slogan">
+              Hi,
+              <br />
+              I'm <span className="highlight">Patrick</span>
+              <br />
+              Software Engineer
+            </p>
             <a
-              href="https://github.com/thminggg"
-              target="_blank"
-              rel="noreferrer"
+              href="#contact"
+              className="contact mt-6 mb-6"
+              onClick={(e) => handleLinkClick(e, "#contact")}
             >
-              <FaGithub className="inline-block w-5 h-5" />
+              Contact
             </a>
-            <a
-              href="https://www.linkedin.com/in/patrick-tse-ca/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaLinkedin className="inline-block w-5 h-5" />
-            </a>
+            <div className="flex gap-3 mt-3">
+              <a
+                href="https://github.com/thminggg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub className="inline-block w-5 h-5" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/patrick-tse-ca/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedin className="inline-block w-5 h-5" />
+              </a>
+            </div>
           </div>
         </div>
-        <img
-          src="sitting.svg"
-          alt="propic"
-          className="hidden w-[25%] h-auto md:block"
-        />
       </div>
     </section>
   );
